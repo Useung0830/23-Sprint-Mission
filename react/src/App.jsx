59ce57f307396f4f037b90a34cf5194d23b14419
Sung_yuseung <usngsung0830@naver.com>
2026-03-24 13:06:03 +0900

    refactor: 파일 이름 변경

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,6 @@
 import ItemsPage from "./pages/ItemsPage";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "./components/Items/Header";
+import Footer from "./components/Items/Footer";
 
 function App() {
   return (
