db3307bf50dc3180f78173b691515f05012569ec
Sung_yuseung <usngsung0830@naver.com>
2026-03-25 23:13:11 +0900

    feat: 파일 이미지 인풋 구현

--- react/src/components/Additem/ImgAdditem.jsx
+++ react/src/components/Additem/ImgAdditem.jsx
@@ -1,9 +1,66 @@
-function ImgAdditem() {
+import { useEffect, useRef, useState } from "react";
+import placeholderImage from "../../assets/placeholderImage.svg";
+import styles from "./ImgAdditem.module.css";
+
+function ImgAdditem({ name }) {
+  const [file, setFile] = useState();
+  const [preview, setPreview] = useState();
+  const inputRef = useRef();
+
+  const handleChange = (e) => {
+    const nextFile = e.target.files[0];
+    setFile(nextFile);
+  };
+
+  const handleClear = () => {
+    setFile(null);
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  };
+
+  const handleClick = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  };
+
+  useEffect(() => {
+    if (!file) {
+      setPreview(null);
+      return;
+    }
+    const objectUrl = URL.createObjectURL(file);
+    setPreview(objectUrl);
+
+    return () => {
+      URL.revokeObjectURL(objectUrl);
+    };
+  }, [file]);
+
   return (
     <div>
       <span>상품 이미지</span>
       <div>
-        <span>이미지 등록</span>
+        <input
+          name={name}
+          type="file"
+          onChange={handleChange}
+          ref={inputRef}
+          hidden
+        />
+
+        <img
+          className={styles.fileImg}
+          src={placeholderImage}
+          onClick={handleClick}
+        />
+        {file && (
+          <div>
+            <img src={preview} className={styles.fileImg} />{" "}
+            <div onClick={handleClear}>X</div>
+          </div>
+        )}
       </div>
     </div>
   );
