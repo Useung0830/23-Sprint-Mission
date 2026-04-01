1b9f56262fbfeb55311dd4d5ba6d547f8ac01098
Sung_yuseung <usngsung0830@naver.com>
2026-04-01 21:45:16 +0900

    feat: 상품등록 버튼 클릭시 /additem 주소로 이동

--- react/src/components/Items/AllList.jsx
+++ react/src/components/Items/AllList.jsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./AllList.module.css";
 import ButtonCreateItem from "./ButtonCreateItem.jsx";
 import InputSearch from "./InputSearch.jsx";
@@ -5,12 +6,16 @@ import ListItem from "./ListItem.jsx";
 import OrderSelect from "./OrderSelect.jsx";
 
 function AllList({ items, type, search, setOrder }) {
+  let navigate = useNavigate();
+
   return (
     <div className={styles.allList}>
       <div className={styles.topSection}>
         <h1 className={styles.title}>전체 상품</h1>
         <InputSearch search={search} />
-        <ButtonCreateItem>상품 등록하기</ButtonCreateItem>
+        <ButtonCreateItem onClick={() => navigate("/additem")}>
+          상품 등록하기
+        </ButtonCreateItem>
         <OrderSelect setOrder={setOrder} />
       </div>
       <ul className={styles.card}>
