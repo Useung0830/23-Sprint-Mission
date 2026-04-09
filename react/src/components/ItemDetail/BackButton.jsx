805e0061152a8cdc28083724e72693a831265869
Sung_yuseung <usngsung0830@naver.com>
2026-04-09 23:39:34 +0900

    style: 목록으로 돌아가기 버튼 스타일 추가

--- react/src/components/ItemDetail/BackButton.jsx
+++ react/src/components/ItemDetail/BackButton.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import ic_back from "../../assets/ic_back.svg";
+import styles from "./BackButton.module.css";
 
 function BackButton() {
   const navigate = useNavigate();
@@ -9,9 +10,9 @@ function BackButton() {
   };
 
   return (
-    <div onClick={handleBack}>
-      <span>목록으로 돌아가기</span>
-      <img src={ic_back} alt="back" />
+    <div className={styles.backButtonWrapper} onClick={handleBack}>
+      <span className={styles.backButtonText}>목록으로 돌아가기</span>
+      <img src={ic_back} alt="back" className={styles.backIcon} />
     </div>
   );
 }
